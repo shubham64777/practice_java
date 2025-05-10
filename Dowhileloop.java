65c79dd54f0f66e9e883ee7127e96393c0328fa7
SHUBHAM KUMAR <152736327+shubham64777@users.noreply.github.com>
2025-05-11 04:23:34 +0530

    Add files via upload
    added practice files of while and do while loops

---
+++ Dowhileloop.java
@@ -0,0 +1,14 @@
+package hello;
+
+public class Dowhileloop {
+
+	public static void main(String[] args) {
+     int number = 5;
+     int n = 1;
+     do {
+    	 System.out.printf("%d * %d = %d\n",number,n,number*n);
+    	 n++;
+     }while(n<=10);
+	}
+
+}
---
+++ Whileloop.java
@@ -0,0 +1,16 @@
+package hello;
+
+public class Whileloop {
+
+	public static void main(String[] args) {
+     int n = 10;
+     int i = 1;
+     int sum = 0;
+     while(i<=n) {
+    	 sum += i;
+    	 i++;
+     }
+     System.out.println("sum of the integers is "+ sum);
+	}
+
+}

869ecebcd26d51b576c9e13167f733890664e256
SHUBHAM KUMAR <152736327+shubham64777@users.noreply.github.com>
2025-05-11 03:45:42 +0530

    Add files via upload
    Added buffered reader practice file

---
+++ BR.java
@@ -0,0 +1,16 @@
+package hello;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class BR {
+
+	public static void main(String[] args) {
+		InputStreamReader in = new InputStreamReader(System.in);
+		BufferedReader bf = new BufferedReader(in);
+		System.out.print("enter the number of your choice:");
+		int n = Integer.parseInt(bf.readLine());
+		System.out.printf("number that you entered is %d", n);
+		bf.close();
+	}
+
+}

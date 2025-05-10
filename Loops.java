b04d7ab251792fa422862c256caaf4b594e13eee
SHUBHAM KUMAR <152736327+shubham64777@users.noreply.github.com>
2025-05-11 04:05:25 +0530

    Add files via upload
    added files of for loop

---
+++ Loops.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class Loops {
+
+	public static void main(String[] args) {
+    int n = 5;
+    for(int number = 1 ; number <=10 ; number++) {
+    	System.out.printf("%d * %d = %d\n",n,number,n*number);
+    }
+	}
+
+}
---
+++ Loops2.java
@@ -0,0 +1,14 @@
+package hello;
+
+public class Loops2 {
+
+	public static void main(String[] args) {
+     int numbers[] = {1,2,3,4,5};
+     int sum = 0;
+     for(int number : numbers) {
+    	 sum+=number;
+     }
+     System.out.println(sum);
+	}
+
+}

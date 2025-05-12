4d863f524853a96910913f589c2f708953d91829
SHUBHAM KUMAR <152736327+shubham64777@users.noreply.github.com>
2025-05-12 19:09:27 +0530

    Update BR.java

--- BR.java
+++ BR.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 public class BR {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		InputStreamReader in = new InputStreamReader(System.in);
 		BufferedReader bf = new BufferedReader(in);
 		System.out.print("enter the number of your choice:");
